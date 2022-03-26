c6a93e4a28893ec3a4d913dc1c3b1bf117fc7bcb
DiorAbjalilov <abjalilovdiyor@gmail.com>
2022-03-26 19:27:40 +0500

    add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 function App() {
-  return <div className="container">hello</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">FORM</div>
+      </div>
+      <div className="row">
+        <div className="col">POST</div>
+        <div className="col">FETCHED POST</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
