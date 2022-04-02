52fb67101cf21f3c55920400a33ed78dea69dc49
DiorAbjalilov <abjalilovdiyor@gmail.com>
2022-04-02 18:45:39 +0500

    aaa

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,7 +3,7 @@ import React from "react";
 const Post = ({ post }) => {
   return (
     <>
-      <div className="card" key={post.id}>
+      <div className="card mt-2" key={post.id}>
         <div className="card-body">
           <h5 className="card-title">{post.title}</h5>
         </div>
